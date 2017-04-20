e63e7b69f1e3c3a9b22329872e973f5bfc1fb5a0
zhongdaoyou <18942371859@163.com>
2017-04-21 00:17:52 +0800

    all the things

---
+++ app/models/job.rb
@@ -0,0 +1,30 @@
+class Job < ApplicationRecord
+  validates :title, presence: true
+  validates :wage_upper_bound, presence: true
+  validates :wage_lower_bound, presence: true
+  validates :wage_lower_bound, numericality: { greater_than: 0}
+
+  has_many :resumes
+
+  def render_job_status(job)
+  if job.is_hidden
+    "(Hidden)"
+  else
+    "(Public)"
+  end
+end
+
+  def publish!
+    self.is_hidden = false
+    self.save
+  end
+
+  def hide!
+    self.is_hidden = true
+    self.save
+  end
+
+  scope :published, -> { where(is_hidden: false) }
+  scope :recent, -> { order('created_at DESC') }
+
+end
---
+++ config/routes.rb
@@ -0,0 +1,22 @@
+Rails.application.routes.draw do
+  devise_for :users
+
+  resources :jobs do
+    resources :resumes
+  end
+
+
+  namespace :admin do
+    resources :jobs do
+      member do
+        post :publish
+        post :hide
+      end
+      resources :resumes
+    end
+  end
+
+
+
+  root 'welcome#index'
+end
